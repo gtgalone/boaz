7ea64fb9d5d4173776a822292ec72210672af5e0
gtgalone <sjhgenius@gmail.com>
2018-10-19 19:43:56 -0400

    Start first for boaz

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react'
+import throttle from 'lodash/throttle'
+
+interface Props {
+  isMobile: boolean
+}
+
+class Index extends React.Component<Props> {
+  private ticking: boolean
+  private mainImageRef: any
+  private heart2Ref: any
+
+  public constructor(props) {
+    super(props)
+    this.mainImageRef = React.createRef()
+    this.heart2Ref = React.createRef()
+  }
+
+  public componentDidMount() {
+    const { isMobile } = this.props
+    console.log(isMobile)  
+    this.mainImageRef.current.style.transform = 'scale(1)'
+    // window.addEventListener(
+    //   'scroll',
+    //   throttle(this.handleScroll, 99)
+    // )
+  }
+  
+  public componentWillUnmount() {
+    // window.removeEventListener(
+    //   'scroll',
+    //   throttle(this.handleScroll, 99)
+    // )
+  }
+
+  public handleScroll = () => {
+    console.log(window)
+    console.log(window.scrollY / document.body.clientHeight)
+    if (!this.ticking) {
+      window.requestAnimationFrame(() => {
+        this.mainImageRef.current.style.opacity = `${window.scrollY / document.body.clientHeight}`
+        this.heartRef.current.style.transform = `rotate(30deg) scale(${window.scrollY / document.body.clientHeight + 0.1})`
+        this.heart2Ref.current.style.opacity = `${window.scrollY / document.body.clientHeight}`
+        this.heart2Ref.current.style.transform = `rotate(-30deg) scale(${window.scrollY / document.body.clientHeight + 0.3})`
+        this.ticking = false
+      })
+
+      this.ticking = true
+    }
+  }
+
+  public render() {
+    const { isMobile } = this.props
+    return (
+      <div className="w-100">
+        <div className="main relative flex justify-center items-center vh-100">
+          <img src="/static/main-image.jpg" ref={this.mainImageRef} className="main-image absolute mw-none" />
+          <div className="background-shadow absolute"></div>
+          <div className="main-image-contents relative flex flex-column justify-center items-center">
+            <div className="main-image-text relative flex flex-wrap justify-center mb6">
+              <div className="white f3 f2-m f1-ns fw9 georgia w-50-ns tc">To break the cycle of poverty, we must embrace the will to navigate <span className="yellow">Upstream</span></div>
+            </div>
+            <div className="white flex flex-column items-center">
+              <i className="arrow-down f2 fw4 fas fa-long-arrow-alt-down mb3"></i>
+              <span className="helvetica o-50 f6">S C R O L L</span>
+            </div>
+          </div>
+        </div>
+        <section className="content w-100 pa4 tc relative bg-white vh-50">
+        
+        </section>
+        <style jsx>{`{
+          hr {
+            border: 1px solid #FFFFFF;
+            flex-grow: 1;
+            flex-shrink: 0;
+            flex-basis: auto;
+          }
+          section {
+            font-family: 'Noto Serif KR', sans-serif !important;
+          }
+          .main-image {
+            transform: scale(3);
+            transition: all 10s;
+          }
+          .main-image-contents {
+            z-index: 2
+          }
+          .background-shadow {
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(56, 49, 38, 0.8);
+            z-index: 1;
+          }
+          @keyframes bounce {
+            0%, 16.667%, 33.333%, 53.33%, 67.667% {
+              transform: translateY(0);
+            }
+            26.667% {
+              transform: translateY(-30px);
+            }
+            40% {
+              transform: translateY(-15px);
+            }
+          }
+          .arrow-down {
+            animation: bounce 3s infinite;
+          }
+          .content {
+            z-index: 2;
+          }
+        }`}</style>
+      </div>
+    )
+  }
+}
+
+export default Index
